82670c47fcbf13abc4f52a7d0028cd1ede9edb7b
Zami17 <zamzamsyifa10@gmail.com>
2023-04-04 02:29:56 +0700

    usaha

---
+++ Unitusaha/app/Http/Controllers/UnitUsahaController.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+
+class UnitusahaController extends Controller
+{
+    public function index()
+    {
+    	// mengambil data dari table 'usaha'
+    	 $usaha = DB::table('usaha')->get();
+ 
+    	// mengirim data 'usaha ke view index
+    	return view('UnitUsaha',[ 'usaha' =>  $usaha]);
+ 
+    }
+    public function tambah()
+    {
+    
+        // memanggil view tambah
+        return view('tambah');
+    
+    }
+
+    // method untuk insert data ke table usaha
+    public function store(Request $request)
+    {
+        // insert data ke table usaha
+        DB::table('usaha')->insert([
+            'nama_usaha' => $request->nama_usaha,
+            'alamat' => $request->alamat,
+            'telepon' => $request->telepon,
+            'jenis_usaha' => $request->jenis_usaha,
+        ]);
+        // alihkan halaman ke halaman usaha
+        return redirect('/UnitUsaha');
+    
+    }
+    // method untuk edit data usaha
+    public function edit($Id_usaha)
+    {
+        // mengambil data usaha berdasarkan id yang dipilih
+        $usaha = DB::table('usaha')->where('Id_usaha',$Id_usaha)->get();
+        // passing data usaha yang didapat ke view edit.blade.php
+        return view('edit',['usaha' => $usaha]);
+    
+    }
+
+    // update data usaha
+    public function update(Request $request)
+    {
+        // update data usaha
+        DB::table('usaha')->where('id_usaha',$request->id_usaha)->update([
+            'nama_usaha' => $request->nama_usaha,
+            'alamat' => $request->alamat,
+            'telepon' => $request->telepon,
+            'jenis_usaha' => $request->jenis_usaha,
+        ]);
+        // alihkan halaman ke halaman usaha
+        return redirect('/UnitUsaha');
+    }
+
+    // method untuk hapus data usaha
+    public function hapus($id_usaha)
+    {
+        // menghapus data usaha berdasarkan id yang dipilih
+        DB::table('usaha')->where('id_usaha',$id_usaha)->delete();
+            
+        // alihkan halaman ke halaman usaha
+        return redirect('/UnitUsaha');
+    }
+}
---
+++ Unitusaha/app/Http/Controllers/jj.php
@@ -0,0 +1,81 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class UnitusahaController extends Controller
+{
+    public function index()
+    {
+    	// mengambil data dari table Surat
+    	$Surat = DB::table('Surat')->get();
+ 
+    	// mengirim data Surat ke view index
+    	return view('UnitUsaha',['Surat' => $Surat]);
+ 
+    }
+    
+    // method untuk menampilkan view form tambah Surat
+    public function tambah()
+    {
+    
+        // memanggil view tambah
+        return view('tambah');
+    
+    }
+
+    // method untuk insert data ke table Surat
+    public function store(Request $request)
+    {
+        // insert data ke table Surat
+        DB::table('Surat')->insert([
+            'Id_Surat' => $request->Id_Surat,
+            'Jenis_Surat' => $request->Jenis_Surat,
+            'Desc_Surat' => $request->Desc_Surat,
+            'Jenis_Tabungan' => $request->Jenis_Tabungan,
+            'Jenis_Simpanan' => $request->Jenis_Simpanan,
+            'Status' => $request->Status
+        ]);
+        // alihkan halaman ke halaman Surat
+        return redirect('/Surat');
+    
+    }
+
+    // method untuk edit data Surat
+    public function edit($Id_Surat)
+    {
+        // mengambil data Surat berdasarkan id yang dipilih
+        $Surat = DB::table('Surat')->where('Id_Surat',$Id_Surat)->get();
+        // passing data Surat yang didapat ke view edit.blade.php
+        return view('edit',['Surat' => $Surat]);
+    
+    }
+
+    // update data Surat
+    public function update(Request $request)
+    {
+        // update data Surat
+        DB::table('Surat')->where('Id_Surat',$request->Id_Surat)->update([
+            'Jenis_Surat' => $request->Jenis_Surat,
+            'Desc_Surat' => $request->Desc_Surat,
+            'Jenis_Tabungan' => $request->Jenis_Tabungan,
+            'Jenis_Simpanan' => $request->Jenis_Simpanan,
+            'Status' => $request->Status,
+        ]);
+        // alihkan halaman ke halaman Surat
+        return redirect('/Surat');
+    }
+
+    // method untuk hapus data Surat
+    public function hapus($Id_Surat)
+    {
+        // menghapus data Surat berdasarkan id yang dipilih
+        DB::table('Surat')->where('Id_surat',$Id_Surat)->delete();
+            
+        // alihkan halaman ke halaman Surat
+        return redirect('/Surat');
+    }
+}
