b4bd71a5684eec6321576f1b1ed312c1aba87f22
rifkautsarfahaddits <89372304+rifkautsarfahaddits@users.noreply.github.com>
2023-06-09 03:45:54 +0700

    Update footer.blade.php

--- Unitusaha/resources/views/layouts/footers/guest/footer.blade.php
+++ Unitusaha/resources/views/layouts/footers/guest/footer.blade.php
@@ -7,20 +7,6 @@
       </div>
         @if (!auth()->user() || \Request::is('static-sign-up')) 
           <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-              <a href="https://dribbble.com/creativetim" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-dribbble" aria-hidden="true"></span>
-              </a>
-              <a href="https://twitter.com/CreativeTim" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-twitter" aria-hidden="true"></span>
-              </a>
-              <a href="https://www.instagram.com/creativetimofficial/" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-instagram" aria-hidden="true"></span>
-              </a>
-              <a href="https://ro.pinterest.com/thecreativetim/" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-pinterest" aria-hidden="true"></span>
-              </a>
-              <a href="https://github.com/creativetimofficial" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-github" aria-hidden="true"></span>
               </a>
           </div>
         @endif
