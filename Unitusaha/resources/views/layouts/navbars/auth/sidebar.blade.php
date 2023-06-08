350490490551819ef668215c6a40ee618bb3103c
rifkautsarfahaddits <89372304+rifkautsarfahaddits@users.noreply.github.com>
2023-06-09 02:55:48 +0700

    Update sidebar.blade.php

--- Unitusaha/resources/views/layouts/navbars/auth/sidebar.blade.php
+++ Unitusaha/resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -223,3 +223,21 @@
       </li>
     </ul>
   </div>
+  
+  <div class="sidenav-footer mx-3 ">
+    <div class="card card-background shadow-none card-background-mask-secondary" id="sidenavCard">
+      <div class="full-background" style="background-image: url('../assets/img/curved-images/white-curved.jpeg')"></div>
+      <div class="card-body text-start p-3 w-100">
+        <div class="icon icon-shape icon-sm bg-white shadow text-center mb-3 d-flex align-items-center justify-content-center border-radius-md">
+          <i class="ni ni-diamond text-dark text-gradient text-lg top-0" aria-hidden="true" id="sidenavCardIcon"></i>
+        </div>
+        <div class="docs-info">
+          <h6 class="text-white up mb-0">Need help?</h6>
+          <p class="text-xs font-weight-bold">Please contact us</p>
+          <a href="#" target="_blank" class="btn btn-white btn-sm w-100 mb-0">Click here</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</aside>
+
