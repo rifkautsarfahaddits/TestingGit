2ee504d30a323b44f2125c3a50a25b84054fc657
rifkautsarfahaddits <89372304+rifkautsarfahaddits@users.noreply.github.com>
2023-06-09 03:39:42 +0700

    Update footer.blade.php

--- Unitusaha/resources/views/layouts/footers/guest/footer.blade.php
+++ Unitusaha/resources/views/layouts/footers/guest/footer.blade.php
@@ -4,37 +4,6 @@
       <div class="row">
       <div class="col-lg-8 mb-4 mx-auto text-center">
           </a>
-      </div>
-        @if (!auth()->user() || \Request::is('static-sign-up')) 
-          <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-              <a href="https://dribbble.com/creativetim" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-dribbble" aria-hidden="true"></span>
-              </a>
-              <a href="https://twitter.com/CreativeTim" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-twitter" aria-hidden="true"></span>
-              </a>
-              <a href="https://www.instagram.com/creativetimofficial/" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-instagram" aria-hidden="true"></span>
-              </a>
-              <a href="https://ro.pinterest.com/thecreativetim/" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-pinterest" aria-hidden="true"></span>
-              </a>
-              <a href="https://github.com/creativetimofficial" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-github" aria-hidden="true"></span>
-              </a>
-          </div>
-        @endif
-      </div>
-      @if (!auth()->user() || \Request::is('static-sign-up')) 
-        <div class="row">
-          <div class="col-8 mx-auto text-center mt-1">
-            <p class="mb-0 text-secondary">
-              Copyright © <script>
-                document.write(new Date().getFullYear())
-              </script> Soft by 
-              <a style="color: #252f40;" href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
-              &
-              <a style="color: #252f40;" href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">UPDIVISION</a>.
             </p>
           </div>
         </div>
