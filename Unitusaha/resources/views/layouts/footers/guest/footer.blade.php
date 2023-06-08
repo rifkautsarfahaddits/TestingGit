46c75adc93ef1212ab0e7f09e4f0f536433d08f5
rifkautsarfahaddits <89372304+rifkautsarfahaddits@users.noreply.github.com>
2023-06-09 03:38:00 +0700

    Update footer.blade.php

--- Unitusaha/resources/views/layouts/footers/guest/footer.blade.php
+++ Unitusaha/resources/views/layouts/footers/guest/footer.blade.php
@@ -3,23 +3,6 @@
     <div class="container">
       <div class="row">
       <div class="col-lg-8 mb-4 mx-auto text-center">
-          <a href="https://www.creative-tim.com/?_ga=2.242299972.757293697.1638911086-1528502635.1638911086" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Company
-          </a>
-          <a href="https://www.creative-tim.com/presentation" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              About Us
-          </a>
-          <a href="https://www.creative-tim.com/presentation" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Team
-          </a>
-          <a href="https://www.creative-tim.com/templates" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Products
-          </a>
-          <a href="https://www.creative-tim.com/blog" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Blog
-          </a>
-          <a href="https://www.creative-tim.com/support-terms" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Pricing
           </a>
       </div>
         @if (!auth()->user() || \Request::is('static-sign-up')) 
