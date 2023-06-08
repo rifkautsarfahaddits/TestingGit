fd7354d7d0698dd772a89ce0b4803a8a26c381d6
rifkautsarfahaddits <89372304+rifkautsarfahaddits@users.noreply.github.com>
2023-06-09 03:46:42 +0700

    Update footer.blade.php

--- Unitusaha/resources/views/layouts/footers/guest/footer.blade.php
+++ Unitusaha/resources/views/layouts/footers/guest/footer.blade.php
@@ -15,12 +15,6 @@
         <div class="row">
           <div class="col-8 mx-auto text-center mt-1">
             <p class="mb-0 text-secondary">
-              Copyright © <script>
-                document.write(new Date().getFullYear())
-              </script> Soft by 
-              <a style="color: #252f40;" href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
-              &
-              <a style="color: #252f40;" href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">UPDIVISION</a>.
             </p>
           </div>
         </div>
